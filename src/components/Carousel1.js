82259b8cbe49f04b185b117142652cca2cad52e9
jaimechico07 <jaimechico14@gmail.com>
2022-06-03 12:14:52 -0500

    armando-header-web

---
+++ src/components/Carousel1.js
@@ -0,0 +1,11 @@
+
+
+function Carousel1() {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default Carousel1;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,37 @@
-
-
+import './Header.css';
+import Carousel1 from './Carousel1';
 
 function Header() {
-    return (
-      <div className="App">
-        <header>
-          <h1>Hola mundo</h1>
-        </header>
-      </div>
-    );
-  }
-  
-  export default Header;
+  return (
+    <div className="Header">
+      <header>
+        <div className="site_header">
+          <div className="logo"></div>
+          <div className="delivery_num"></div>
+        </div>
+
+        <div className="nav_container">
+          <div className="nav_container-menu max-container">
+            <nav className="nav_menu">
+              <a href="/page/Nosotros.js">NOSOTROS</a>
+              <a href="/page/Promociones.js">PROMOCIONES</a>
+              <a href="/page/Cerveza.js">CERVEZAS</a>
+              <a href="/page/Reparto.js">ZONAS DE REPARTO</a>
+              <a href="/page/Contacto.js">CONTACTO</a>
+            </nav>
+          </div>
+
+          <div>
+            <div></div>
+            <button></button>
+          </div>
+        </div>
+        
+      </header>
+
+      <Carousel1 />
+    </div>
+  );
+}
+
+export default Header;
