a5b5bd09e1c21215d8770f7e80695af8a2d80531
jaimechico07 <jaimechico14@gmail.com>
2022-06-03 11:14:13 -0500

    agregando-header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+
+
+
+function Header() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Hola mundo</h1>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Header;
